ec31d7421d845a67564998608a00a7d133d3b120
Pouria Jahandideh <pjsofts@gmail.com>
2023-11-21 14:37:54 +0330

    post

--- day4/web2/services/user.py
+++ day4/web2/services/user.py
@@ -1,6 +1,12 @@
 from models.user import Users
+from app import db
 
 def get_users():
     users = Users.query.all()
     result = [{"name": user.name, "lastname": user.lastname} for user in users]
     return result
+
+def add_user(name, lastname):
+    user = Users(name=name, lastname=lastname)
+    db.session.add(user)
+    db.session.commit()
--- day4/web2/views/user.py
+++ day4/web2/views/user.py
@@ -1,7 +1,17 @@
 from app import app
-from services.user import get_users
+from services.user import get_users, add_user
+from flask import request
 
 @app.route("/users", methods=["GET"])
 def index():
     users = get_users()
     return users
+
+
+@app.route("/users", methods=["POST"])
+def add_posts():
+    data = request.get_json()
+    name = data.get("name")
+    lastname = data.get("lastname")
+    add_user(name, lastname)
+    return "OK"
