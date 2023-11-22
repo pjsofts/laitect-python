13978fa21a2af2518851edb48a0e82a16aec0224
Pouria Jahandideh <pjsofts@gmail.com>
2023-11-22 13:49:43 +0330

    mongo fastapi

--- day4/web2/services/todo.py
+++ day4/web2/services/todo.py
@@ -6,7 +6,9 @@ from app import db
 def get_todos():
     todos = Todos.query.all()
     result = [
-        {"title": todo.title, "done": todo.done, 
+        {
+            "id": todo.id,
+            "title": todo.title, "done": todo.done, 
         "user_id": todo.users.id,
          "user_name": todo.users.name, 
         "user_lastname": todo.users.lastname}
